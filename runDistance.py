a14711208685eca7fa5c3359621ee5eb007d91d2
juliuso <julius.o@gmail.com>
2019-08-14 05:41:46 -0400

    Added partitionOnZeroSumAndMax()

--- runDistance.py
+++ runDistance.py
@@ -3,6 +3,17 @@
 # DATE: 2019-08-12
 # DESCRIPTION: Tuse homework assignment #1.
 
+def partitionOnZeroSumAndMax(row):
+    # Ex. Passing in [1,1,0,1] returns [2,0,1].
+    # max([2,0,1]) -> 2
+    localSums = [0]
+    for val in row:
+        if val != 0:
+            localSums.append(localSums.pop() + val)
+        else:
+            localSums.append(0)
+    return max(localSums)
+
 def getDistanceAndMax(matrix):
 
     # Initialize empty array with identical dimensions as input.
@@ -31,7 +42,8 @@ def getDistanceAndMax(matrix):
                 matrix_distance[row][column] = distance
 
     return {'distance' : matrix_distance,
-            'max_score': max(sum(row) for row in matrix_distance)
+            'max_score': max(partitionOnZeroSumAndMax(row)
+                             for row in matrix_distance)
            }
 
 if __name__ == '__main__':
@@ -48,5 +60,17 @@ if __name__ == '__main__':
         [0, 1, 0]
     ]
 
+    tootSnoots = [1,1,0,1]
+
+    toodles = [
+        [1,1,0,1],
+        [1,1,0,1],
+        [1,1,0,1],
+        [1,1,0,1]
+    ]
+    
     print(getDistanceAndMax(snoots))
     print(getDistanceAndMax(winkler))
+    print(partitionOnZeroSumAndMax(tootSnoots))
+    print(getDistanceAndMax(toodles))
+
